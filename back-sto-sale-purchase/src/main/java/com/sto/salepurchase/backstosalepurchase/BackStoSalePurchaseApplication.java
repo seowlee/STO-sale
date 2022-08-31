53d291e81d772d98d0ab6495ede3bce1e3547063
이서우 <seowlee@iseouui-MacBookPro.local>
2022-08-31 11:30:20 +0900

    Create back-sto-sale-purchase directory
    backend springboot mysql

---
+++ back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/BackStoSalePurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.sto.salepurchase.backstosalepurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStoSalePurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStoSalePurchaseApplication.class, args);
+	}
+
+}
