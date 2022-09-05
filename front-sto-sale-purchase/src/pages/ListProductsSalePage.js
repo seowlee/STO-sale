b71d68d052a362af2b601cc233bbd945ca92614c
Seo Woo Lee <tegfsl@gmail.com>
2022-09-05 19:55:37 +0900

    FEAT: make Card component in ListProductsSalePage

--- front-sto-sale-purchase/src/pages/ListProductsSalePage.js
+++ front-sto-sale-purchase/src/pages/ListProductsSalePage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Divider } from "@mui/material";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  ></Box>
+);
 
 const ListProductsSalePage = () => {
-  return <div>ListProductsSalePage</div>;
+  return (
+    <div>
+      <br />
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default ListProductsSalePage;
