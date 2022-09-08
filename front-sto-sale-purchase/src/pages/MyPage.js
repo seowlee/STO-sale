84d79e544f792bd379e2e4ca2843db421892e36e
Seo Woo Lee <tegfsl@gmail.com>
2022-09-08 20:50:15 +0900

    FEAT: add Card Component in MyPage

--- front-sto-sale-purchase/src/pages/MyPage.js
+++ front-sto-sale-purchase/src/pages/MyPage.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Button,
+  Typography,
+} from "@mui/material";
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <br />
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            benevolent
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        {/* <Divider />
+        <CardActions>
+          <Button size="large">구매하기</Button>
+        </CardActions> */}
+      </Card>
+    </div>
+  );
 };
 
 export default MyPage;
