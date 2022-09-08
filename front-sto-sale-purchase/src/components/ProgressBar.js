1cc86f496c14337a663fac7b52c2a2cd1203088c
Seo Woo Lee <tegfsl@gmail.com>
2022-09-08 20:36:38 +0900

    FEAT: add a ProgressBar in ListProductsSalePage

---
+++ front-sto-sale-purchase/src/components/ProgressBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Box, LinearProgress, Typography } from "@mui/material";
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
+
+export default function LinearWithValueLabel() {
+  const [progress, setProgress] = React.useState(10);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgressWithLabel value={progress} />
+    </Box>
+  );
+}
