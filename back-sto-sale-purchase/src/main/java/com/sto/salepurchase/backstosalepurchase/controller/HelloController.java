2ca3e1091f004925597d17769b4f0c381da1d92e
seowlee <seowlee@iseouui-MacBookPro.local>
2022-09-13 15:56:19 +0900

    STYLE: saparating folders (controller, service, repository, domain)

--- back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/controller/HelloController.java
+++ back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.sto.salepurchase.backstosalepurchase;
+package com.sto.salepurchase.backstosalepurchase.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +8,8 @@ import java.util.List;
 
 @RestController
 public class HelloController {
-	@GetMapping("hello")
-	public List<String> Hello() {
-		return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
-	}
+    @GetMapping("hello")
+    public List<String> Hello() {
+        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+    }
 }
