3574b675e89869ad8ce2eb2ec08a3f39c25fb674
Seo Woo Lee <tegfsl@gmail.com>
2022-09-22 00:36:41 +0900

    FEAT: make entity class of mysql rds goods table

--- back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/domain/Product.java
+++ back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/domain/Product.java
@@ -1,5 +1,6 @@
 package com.sto.salepurchase.backstosalepurchase.domain;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,16 +8,39 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "goods")
 @Entity
 public class Product {
-    @Id
-    private String id;
-    private String name;
-    private String password;
+	@Id
+	private Long goods_id;
+	@NotNull
+	private String goods_nm;
+
+	@NotNull
+	private Integer stat;
+	private Integer total_amt;
+	private Integer sale_amt;
+	private Integer total_cnt;
+	private Double ordr_fee;
+	private Double trade_fee;
+	private Double sale_fee;
+
+	// java.sql
+	private Timestamp created_dt;
+	private Timestamp updated_dt;
+
+	@Size(max = 20)
+	private String created_by;
+	@Size(max = 20)
+	private String updated_by;
+
 
 }
--- back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/repository/ProductRepository.java
+++ back-sto-sale-purchase/src/main/java/com/sto/salepurchase/backstosalepurchase/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.sto.salepurchase.backstosalepurchase.repository;
 
 import com.sto.salepurchase.backstosalepurchase.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
