463e4ec035c75c0dc934262e3ac9afd7bf5c2b7e
seowlee <chloe@infiniteblock.kr>
2022-09-23 19:01:28 +0900

    FEAT: change fetch to axios on request

--- front-sto-sale/src/pages/OrderPage.js
+++ front-sto-sale/src/pages/OrderPage.js
@@ -1,6 +1,7 @@
+import axios from "axios";
 import React, { useState, useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { DetailProductContext } from "./ListProductsSalePage";
+// import { DetailProductContext } from "./ListProductsSalePage";
 
 const OrderPage = () => {
   const { goods_id } = useParams();
@@ -8,20 +9,33 @@ const OrderPage = () => {
   const [detailProduct, setDetailProduct] = useState({
     goods_id: "",
     goods_nm: "",
+    stat: 0,
   });
 
+  // useEffect(() => {
+  //   console.log("id : ", goods_id);
+  //   fetch("http://localhost:8080/order/" + goods_id)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("data : ", data);
+  //       setDetailProduct(data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("에러", error);
+  //     });
+  // }, []);
   useEffect(() => {
-    console.log("id : ", goods_id);
-    fetch("http://localhost:8080/order/" + goods_id)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("data : ", data);
-        setDetailProduct(data);
+    axios
+      .get("/order/" + goods_id)
+      .then((res) => {
+        setDetailProduct(res.data);
+        console.log("detailtest", res);
       })
       .catch((error) => {
-        console.log("에러", error);
+        console.log("error", error);
       });
   }, []);
+
   console.log("param", goods_id);
   console.log("Success", detailProduct);
 
@@ -29,6 +43,7 @@ const OrderPage = () => {
     <div>
       <div>{goods_id} 페이지</div>
       <h1>{detailProduct.goods_nm}</h1>
+      <h1>{detailProduct.total_amt}</h1>
     </div>
   );
 };
