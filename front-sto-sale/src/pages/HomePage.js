3f03e13466914f73aa8194d09d197418f4ba0986
seowlee <seowlee@yeongugaebal-iseou.local>
2022-10-12 11:54:13 +0900

    FEAT: Initialize sales info table through post when clicking button

--- front-sto-sale/src/pages/HomePage.js
+++ front-sto-sale/src/pages/HomePage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const handlePostSaleInfo = () => {
+    axios
+      .post(`/sales/all`, {})
+      .then((res) => {})
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+  return (
+    <div>
+      HomePage <br />
+      <br />
+      <Button variant="contained" size="large" onClick={handlePostSaleInfo}>
+        판매정보 초기화
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
